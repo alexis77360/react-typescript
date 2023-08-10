1a6d263f6d7f7f7a7fc5a8d113ce4fdec4072c62
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-08-10 16:49:25 +0200

    typescript form

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,49 @@
-import React, { useRef } from 'react';
-import { useState } from 'react';
+import React from 'react';
+import { useRef } from 'react';
 import { MessageInt } from './model';
 import Message from './components/Message';
 
 const App = () => {
-  //? Grace a useRef on peut récupérer la valeur d'un input et HTMLInputElement est le type de l'input
+
+  //! Le HTMLInputElement est un type générique qui permet de typer les éléments HTML
   const inputMessage = useRef<HTMLInputElement>(null);
-  const [messData, setMessData] = useState<MessageInt[]>([]);
-
-
+  const [messData, setMessData] = React.useState<MessageInt[]>([]);
 
   const handleSubmit = (e:any) => {
     e.preventDefault();
 
     if(inputMessage){
       const mess:MessageInt = {
-        id : Math.round(Math.random() * Date.now()),
-        message : inputMessage?.current?.value,
-        date : Date.now()
-      }
-      //! On récupère les données précédentes et on ajoute le nouveau message
-      setMessData((prevDate) => [...prevDate, mess]);
+        id: Math.round(Math.random() * Date.now()),
+        message: inputMessage?.current?.value,
+        date: Date.now()
     }
-  
+    //! Pour ajouter un message à la liste des messages
+    setMessData((prevMessData) => [...prevMessData, mess]);
+  }
+
+    //! Pour repasser l'input en vide
+    (document.getElementById("inputMessage") as HTMLInputElement).value = "";
 
 
-    //? Logique d'envoie de données
-    //? Reset de l'input avec TypeScript
-  (document.getElementById('inputMessage') as HTMLInputElement).value = '';
+
   };
-
-
 
   return (
     <div>
-      <h1>React TypeScript</h1>
-      <h2> Poster un message</h2>
+      <h2> Poster un message</h2>  
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Votre message" id="inputMessage" ref={inputMessage} />
+        <input type="text" placeholder="Entrer un message" id="inputMessage" ref={inputMessage}/>
         <input type="submit" value="Envoyer" />
       </form>
-      <h2>Messages</h2>
+      <h2> Messages</h2>
       <div> {messData?.map((mess) => (
-        <Message mess={mess} key={mess.id} messData={messData} setMessData={setMessData} /> 
-      ))}
-             
-        </div>
       
+      <Message mess={mess} messData={messData} setMessData={setMessData} key={mess.id} />
+      ))} </div>
+      
+      
+
     </div>
   );
 };
